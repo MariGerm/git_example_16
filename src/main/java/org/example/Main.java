12564dc430798196a39a7bb7b6cfb005d5afba02
Maria Manyk <mariiamanyk@gmail.com>
2022-11-16 16:31:31 +0300

    added new info to println

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello QA Guru Maria!");
+        System.out.println("I wish peace to Ukraine!");
     }
 }
