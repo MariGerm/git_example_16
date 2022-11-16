c69a7db99e72adaca006b0c64854886f301cc079
Maria Manyk <mariiamanyk@gmail.com>
2022-11-16 16:10:48 +0300

    added name to println

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello QA Guru Maria!");
+        System.out.println("I wish peace to Ukraine!")
+
     }
 }
