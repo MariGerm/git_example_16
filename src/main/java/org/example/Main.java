93607a8ffcdcb45cf6be478b0c134a742a226a16
Maria Manyk <mariiamanyk@gmail.com>
2022-11-16 17:05:45 +0300

    Personal data removed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello QA Guru Maria!");
+        System.out.println("Hello QA Guru [Censored]!");
         System.out.println("I wish peace to Ukraine!");
     }
 }
